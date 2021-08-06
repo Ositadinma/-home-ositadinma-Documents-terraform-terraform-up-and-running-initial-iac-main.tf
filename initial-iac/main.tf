b539a98a87b94e64166c21713c32b562d0edc0ea
Ositadinma <ositadinma.eze@matchesfashion.com>
2021-08-06 22:34:24 +0100

    Added security group, output and data resources

--- initial-iac/main.tf
+++ initial-iac/main.tf
@@ -13,10 +13,36 @@ provider "aws" {
   profile = "serverless-admin"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-02b4e72b17337d6c1"
+resource "aws_launch_configuration" "example" {
+  image_id                     = "ami-02b4e72b17337d6c1"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups  = [aws_security_group.instance.id]
+  user_data = <<-EOF
+    # !/bin/bash
+    echo "Hello, world" > index.html
+    nohup busybox httpd -f -p ${var.server_port} &
+  EOF
+
+  # Required when using a launch configuration with an auto scaling group
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
 }
 
 resource "aws_security_group" "instance" {
@@ -40,8 +66,27 @@ variable "server_port" {
   default = 8080
 }
 
-output "outputt-public_server" {
-  description = "Out puts public server ip"
-  value = aws_instance.example.public_ip
-  
+data "aws_vpc" "default" {
+  default = true
 }
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_autoscaling_group" "example" {
+  name = aws_autoscaling_group.example.name
+}
+
+output "autoscalling_group" {
+  description = "Out puts the min and max size of the group"
+  value = <<-EOF
+  The autoscalling group min size is: ${data.aws_autoscaling_group.example.min_size}
+  The autoscalling group max size is: ${data.aws_autoscaling_group.example.max_size}
+  EOF
+}
+
+output "availability_zones" {
+  value = data.aws_autoscaling_group.example.availability_zones
+}
+
